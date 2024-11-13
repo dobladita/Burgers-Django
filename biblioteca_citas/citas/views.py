9395e6d758fe356ebc8d459e614cb07a5d1356df
Camilolero <mena.camilo90@gmail.com>
2024-11-13 15:55:00 -0300

    Avance vista crear cita

--- biblioteca_citas/citas/views.py
+++ biblioteca_citas/citas/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
-
+from .models import Cita
 # Create your views here.
+
+def crear_cita(request):
+    if request.method == "POST":
+        texto = request.POST.get('texto')
+        autor = request.POST.get('autor')
+        fecha = request.POST.get('fecha')
+        fuente_id = request.POST.get('fuente')
